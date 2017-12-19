c4b42398e8cd43bfd176a438f4ed924dd001f239
littlephone <littlerabbit506@gmail.com>
2017-12-19 20:24:50 +0800

    Commit 20171219 - 00003
    [新读者注册 - NewStudRegister.java]
    优化 - 全新的互动界面

--- src/com/lib/linkage/NewStudRegister.java
+++ src/com/lib/linkage/NewStudRegister.java
@@ -19,19 +19,27 @@ FocusListener, MouseListener{
 	JFrame frame;
 	JLabel name = new JLabel("姓名: ");
 	JLabel stdno = new JLabel("学号: ");
+	JLabel idcard = new JLabel("校园卡号: "); 
+	JLabel pw = new JLabel("借书/登录密码: ");
 	
 	JLabel enteredname = new JLabel("<请输入姓名>");
 	JLabel enteredsno = new JLabel("<请输入学号>");
+	JLabel enteredscard = new JLabel("<请输入校园卡号>");
 	
 	JTextField sname = new JTextField();
 	JTextField sno = new JTextField();
+	JTextField scard = new JTextField();
+	JPasswordField password = new JPasswordField();
+	
 	Border border = BorderFactory.createEmptyBorder(10,10,10,10);
 	Border ImageBorder = BorderFactory.createEmptyBorder(10,10,10,10);
 
 	GridBagLayout gbl = new GridBagLayout();
 	GridBagConstraints gc = new GridBagConstraints();
 	JPanel RegisterPanel = new JPanel(gbl);
-
+	
+	String adjustedstcard;
+	
 	public void showRegisterMenu(JFrame frame) {
 		this.frame = frame;
 
@@ -60,6 +68,12 @@ FocusListener, MouseListener{
 		enteredsno.addMouseListener(this);
 		enteredsno.setBorder(border);
 		
+		scard.getDocument().addDocumentListener(this);
+		scard.addFocusListener(this);
+		
+		enteredscard.addMouseListener(this);
+		enteredscard.setBorder(border);
+		
 		gc.gridwidth = GridBagConstraints.REMAINDER;
 		gbl.setConstraints(register,	 gc);
 		RegisterPanel.add(register);
@@ -81,6 +95,7 @@ FocusListener, MouseListener{
 		RegisterPanel.add(enteredname);
 		enteredname.setVisible(false);
 		
+		//Themeing Stdnumber Tag
 		RegisterPanel.add(stdno);
 		sno = TranslucentTextField(sno);     //Style sno TextField
 		
@@ -91,6 +106,26 @@ FocusListener, MouseListener{
 		gbl.setConstraints(enteredsno, gc);
 		RegisterPanel.add(enteredsno);
 		enteredsno.setVisible(false);
+		
+		//Themeing card number Tag
+		RegisterPanel.add(idcard);
+		
+		scard = TranslucentTextField(scard);
+		gbl.setConstraints(scard, gc);
+		scard.setBorder(border);
+		
+		RegisterPanel.add(scard);
+		gbl.setConstraints(enteredscard, gc);
+		RegisterPanel.add(enteredscard);
+		enteredscard.setVisible(false);
+		
+		//Themeimg password number tag
+		RegisterPanel.add(pw);
+		password = TranslucentPwField(password);
+		gbl.setConstraints(password, gc);
+		password.setBorder(border);
+		RegisterPanel.add(password);
+		
 		
 		frame.add(RegisterPanel,BorderLayout.CENTER);
 		frame.revalidate();
@@ -104,6 +139,13 @@ FocusListener, MouseListener{
 		return field;
 	}
 	
+	public JPasswordField TranslucentPwField(JPasswordField field) {
+		field.setMaximumSize(field.getPreferredSize());
+		field.setBackground(new Color(0,0,0,0));
+		field.setOpaque(false);
+		field.setColumns(10);
+		return field;
+	}
 	@Override
 	public void changedUpdate(DocumentEvent arg0) {
 		// TODO Auto-generated method stub
@@ -121,6 +163,14 @@ FocusListener, MouseListener{
 			if(sno.getText().equals("")) enteredsno.setText("<请输入学号>");
 			else enteredsno.setText(sno.getText());
 		}
+		if(de.getDocument() == scard.getDocument()) {
+			if(scard.getText().equals("")) enteredscard.setText("<请输入卡号>");
+			else {
+				System.out.print("Here");
+				adjustedstcard = fillDigit(scard.getText());
+				enteredscard.setText(adjustedstcard);
+			}
+		}
 		frame.revalidate();
 		frame.repaint();
 	}
@@ -135,6 +185,13 @@ FocusListener, MouseListener{
 		if(de.getDocument() == sno.getDocument()) {
 			if(sno.getText().equals("")) enteredsno.setText("<请输入学号>");
 			else enteredsno.setText(sno.getText());
+		}
+		if(de.getDocument() == scard.getDocument()) {
+			if(scard.getText().equals("")) enteredscard.setText("<请输入卡号>");
+			else {
+				adjustedstcard = fillDigit(scard.getText());
+				enteredscard.setText(adjustedstcard);
+			}
 		}
 		frame.revalidate();
 		frame.repaint();
@@ -154,9 +211,22 @@ FocusListener, MouseListener{
 			sno.setVisible(false);
 			enteredsno.setVisible(true);
 		}
-		
+		if(e.getSource() == scard) {
+			scard.setVisible(false);
+			enteredscard.setVisible(true);
+		}
 	}
 
+	public String fillDigit(String str) {
+		//Adding zeros to form an eight digit code
+		int maxlen = 8;
+		int length = str.length();
+		int num_of_zero = maxlen - length;
+		for(int i=num_of_zero; i>0; i--) {
+			str = "0"+str;
+		}
+		return str;
+	}
 	@Override
 	public void mouseClicked(MouseEvent arg0) {}
 	@Override
@@ -174,6 +244,11 @@ FocusListener, MouseListener{
 			sno.setVisible(true);
 			sno.grabFocus();
 			enteredsno.setVisible(false);
+		}
+		if(e.getSource() == enteredscard) {
+			scard.setVisible(true);
+			scard.grabFocus();
+			enteredscard.setVisible(false);
 		}
 	}
 
